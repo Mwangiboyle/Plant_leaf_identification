7a68e209d1c30099aa8caf44ae4e1577e414e19c
Mwangiboyle <mwangiboyle4@gmail.com>
2025-08-09 18:57:38 +0300

    installed fastapi and uvicorn

---
+++ main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+
+app = FastAPI(
+    title="Wlcome to my fastapi ",
+)
+
+@app.get('/')
+def root():
+    return {"Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port = 8000)
+
+
